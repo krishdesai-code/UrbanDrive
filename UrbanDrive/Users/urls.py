d2401861a394357f20f4e6307866ffe696272e5a
Krish Desai <krishdesai183@gmail.com>
2026-03-10 23:37:35 +0530

    User Login and Register system completed

--- UrbanDrive/Users/urls.py
+++ UrbanDrive/Users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("register/",views.Register,name="register"),
+    path("login/",views.login,name="login"),
 ]
--- UrbanDrive/Users/views.py
+++ UrbanDrive/Users/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.hashers import make_password,check_password
+from .models import Users
 
 def Register(request) :
+    if request.method == "POST" :
+        name = request.POST.get("un")
+        email = request.POST.get("email")
+        password = request.POST.get("pass")
+
+        hased_pass = make_password(password)
+
+        Users.objects.create(
+            username=name,
+            email=email,
+            password = hased_pass
+        )
+        return redirect("login")
     return render(request,"users/Register.html")
+
+def login(request) :
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("pass")
+
+        user = Users.objects.get(email=email)
+
+        if check_password(password,user.password):
+            request.session ['username'] = user.username
+            request.session ['email'] = user.email 
+            return redirect("home")
+        else:
+            error = "Invalid Email or password!!"
+            return render(request,'users/login.html',{'error' : error })
+
+    return render(request,"users/login.html")
